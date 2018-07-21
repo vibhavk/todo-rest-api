108002dcf5d127a21b29bc89342620db5ab94fda
vibhavk <vibhavkunj@hotmail.com>
2018-07-21 19:10:27 +0530

    Added the DELETE todos/id route

--- server/server.js
+++ server/server.js
@@ -45,6 +45,19 @@ app.get('/todos/:id',(req,res)=>{
     }).catch((e)=>res.status(400).send());
 });
 
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch((e)=>res.status(400).send());
+});
+
 app.listen(port,()=>{
     console.log(`Starting on port :${port}`);
 });
