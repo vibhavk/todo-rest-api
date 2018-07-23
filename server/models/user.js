620fef1aeb585925c250129e683da623aed627f1
vibhavk <vibhavkunj@hotmail.com>
2018-07-23 17:55:32 +0530

    Initial phase of user authentication and private routing

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,73 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true,
         minlength:1,
-        trim:true
-    }
+        trim:true,
+        unique:true,
+        validate:{
+            validator:validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    tokens:[{
+        access:{
+            required:true,
+            type:String
+        },
+        token:{
+            required:true,
+            type:String
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function(){ //overriding toJSON method to selectively send only safe info back
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject,['_id','email']);
+};
+
+UserSchema.methods.generateAuthToken = function (){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access,token}]);
+
+    return user.save().then(()=>{
+        return token; //the value will get passed as the success argument for next then call
+    });
+};
+
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token,'abc123');
+    } catch(e){
+        return Promise.reject(); //return a rejected promise for catch in server.js to process
+    }
+
+    return User.findOne({ //single quotes are because findOne method when searching for nested properties, require single quotes...added to _id just for consistency!
+        '_id':decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
+var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -11,19 +11,21 @@ console.log(process.env.MONGODB_URI);
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+
 const {ObjectID} = require('mongodb');
-
-
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const _ = require('lodash');
+var {authenticate} = require('./middleware/authenticate');
 
 const port = process.env.PORT;
 
 var app = express();
 
 app.use(bodyParser.json());
+
+////////////TODOS////////////
 
 app.post('/todos',(req,res)=>{
     var todo = new Todo({
@@ -93,6 +95,25 @@ app.patch('/todos/:id',(req,res)=>{
         res.status(400).send();
     });
 
+});
+
+/////USERS/////
+
+app.post('/users',(req,res)=>{
+    var user = new User(_.pick(req.body,['email','password'])); //since _.pick() returns an Object....SHABBASH!
+    user.save().then((user)=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+});
+
+//Authentication middleware comes from middleware/authenticate.js
+
+app.get('/users/me',authenticate,(req,res)=>{
+    res.send(req.user);
 });
 
 app.listen(port,()=>{
